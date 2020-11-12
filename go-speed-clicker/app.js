cf7ee9558a78597329390afe90778326273e9032
Sean Joseph <sean_joseph@protonmail.com>
2020-11-12 12:50:06 -0800

    Adding go speed clicker

---
+++ go-speed-clicker/app.js
@@ -0,0 +1,35 @@
+document.querySelector("#counter p").textContent = localStorage.getItem("clicks");
+
+let clickCount = 0;
+let coundown = 10;
+
+//starts the counter function when page loads and repeats it for 10 seconds
+let timerID = setInterval(counter, 1000);
+
+//stops counter function after 10 seconds
+setTimeout(()=>{clearInterval(timerID);alert("Time's up!")}, 11000 );
+
+window.addEventListener("click", clicker)
+//after 10 seconds this calls cancel function which turns off event listener
+setTimeout(cancel,10000);
+
+function clicker(e){
+    e.preventDefault();
+    //removes previous p element
+    document.querySelector("#counter p").remove();
+    clickCount++;
+    let newP = document.createElement("p");
+    newP.textContent = `You have clicked ${clickCount} time(s)`;
+    document.getElementById("counter").append(newP);
+    localStorage.setItem("clicks",clickCount);
+
+}
+
+function cancel(){
+    window.removeEventListener("click", clicker);
+}
+
+function counter(){
+    document.getElementById("countdownTimer").textContent = coundown;
+    coundown--;
+}
