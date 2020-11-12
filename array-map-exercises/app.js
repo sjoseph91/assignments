92e5cb369e45fce75ed7d970b996dc137a02d3e6
Sean Joseph <sean_joseph@protonmail.com>
2020-11-12 14:39:26 -0800

    Adding Array Map Exercises

---
+++ array-map-exercises/app.js
@@ -0,0 +1,81 @@
+// #1
+
+const numbers = [2, 5, 100];
+
+function doubleNumbers(arr){
+    return arr.map(number=> number * 2);
+    
+}
+console.log(doubleNumbers(numbers));
+
+// #2
+function stringItUp(arr){
+    return arr.map(num => num.toString());
+}
+console.log(stringItUp(numbers));
+
+//#3
+const names = ["john", "JACOB", "jinGleHeimer", "schmidt"];
+
+function capitalizeNames(arr){
+    return arr.map((name) => {
+        
+        const first = name[0].toUpperCase();
+        return first.concat(name.slice(1).toLowerCase());
+    })
+}
+console.log(capitalizeNames(names));
+
+// #4
+const people = [
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+];
+function namesOnly(arr){
+    return arr.map(name =>{
+        return name.name;
+    })
+}
+console.log(namesOnly(people));
+
+// #5
+
+function makeStrings(arr){
+    return arr.map( person => {
+        if (person.age > 20)
+        {
+            return `${person.name} can go to the Matrix`;
+        }
+        else{
+            return `${person.name} is under age`;
+        }
+    })
+}
+console.log(makeStrings(people));
+
+// #6
+function readytoPuInTheDom(arr){
+    return arr.map(person =>{
+        return `<h1>${person.name}</h1><h2>${person.age}</h2>`
+    })
+}
+
+console.log(readytoPuInTheDom(people));
